eefc96ad71759a3c59e0e9c3ffe38b0b44755b33
Javier Castro <javiercastro.ik@gmail.com>
2017-04-08 02:52:32 -0500

    integration vue.js

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,71 +1,74 @@
-(function () {
-    var content = document.getElementById("app");
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (objPosition) {
-            var lon = objPosition.coords.longitude;
-            var lat = objPosition.coords.latitude;
+// var locations = [
+//     ['Bondi Beach', -33.890542, 151.274856, 4],
+//     ['Coogee Beach', -33.923036, 151.259052, 5],
+//     ['Cronulla Beach', -34.028249, 151.157507, 3],
+//     ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+//     ['Maroubra Beach', -33.950198, 151.259302, 1]
+//   ];
 
-            var dir = "";
-            var dpto = "";
-            var latlng = new google.maps.LatLng(lat, lon);
-            geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ "latLng": latlng }, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    if (results[0]) {
-                        console.log(results);
-                        dir = "<p><strong>Dirección: </strong>" + results[0].formatted_address + "</p>";
-                        dpto = "<p><strong>Departamento: </strong>" + results[0].address_components[3].long_name + "</p>";
-                    }
-                    else {
-                        dir = "<p>No se ha podido obtener ninguna dirección en esas coordenadas.</p>";
-                    }
-                }
-                else {
-                    dir = "<p>El Servicio de Codificación Geográfica ha fallado con el siguiente error: " + status + ".</p>";
-                }
+  // var map = new google.maps.Map(document.getElementById('map'), {
+  //   zoom: 10,
+  //   center: new google.maps.LatLng(-33.92, 151.25)
+  //   // mapTypeId: google.maps.MapTypeId.ROADMAP
+  // });
+  //
+  // var infowindow = new google.maps.InfoWindow();
+  //
+  // var marker, i;
+  //
+  // for (i = 0; i < locations.length; i++) {
+  //   marker = new google.maps.Marker({
+  //     position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+  //     map: map
+  //   });
+  //
+  //   google.maps.event.addListener(marker, 'click', (function(marker, i) {
+  //     return function() {
+  //       infowindow.setContent(locations[i][0]);
+  //       infowindow.open(map, marker);
+  //     }
+  //   })(marker, i));
+  // }
 
-                content.innerHTML = "<p><strong>Latitud:</strong> " + lat + "</p><p><strong>Longitud:</strong> " + lon + "</p>" + dir + dpto;
-            });
-        }, function (objPositionError) {
-                switch (objPositionError.code) {
-                    case objPositionError.PERMISSION_DENIED:
-                        content.innerHTML = "No se ha permitido el acceso a la posición del usuario.";
-                        break;
-                    case objPositionError.POSITION_UNAVAILABLE:
-                        content.innerHTML = "No se ha podido acceder a la información de su posición.";
-                        break;
-                    case objPositionError.TIMEOUT:
-                        content.innerHTML = "El servicio ha tardado demasiado tiempo en responder.";
-                        break;
-                    default:
-                        content.innerHTML = "Error desconocido.";
-                }
-            }, {
-                maximumAge: 75000,
-                timeout: 15000
-            });
-    }
-    else {
-        content.innerHTML = "Su navegador no soporta la API de geolocalización.";
-    }
-
-    function initMap() {
-      var uluru = {lat: -12.0973775, lng: -77.05774420000002};
+  var app = new Vue({
+    el: '#vueMap',
+    data: {
+      locations: [
+        {
+          nombre: 'Bondi Beach',
+          latitud: '-33.890542',
+          longitud: '151.274856'
+        },
+        {
+          nombre:'Coogee Beach',
+          latitud:'-33.923036',
+          longitud:'151.259052'
+        }
+      ]
+    },
+    mounted: function(){
       var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 18,
-        center: uluru
+        zoom: 10,
+        center: new google.maps.LatLng(-33.92, 151.25)
       });
-      var infowindow = new google.maps.InfoWindow({
-        content: "<h5>Contenido Estático</h5>"
-      });
-      var marker = new google.maps.Marker({
-          position: uluru,
+      var infowindow = new google.maps.InfoWindow();
+      var marker = [];
+      var i;
+
+      for (i = 0; i < this.locations.length; i++) {
+        console.log(this.locations[i].nombre);
+        var objMarker = new google.maps.Marker({
+          position: new google.maps.LatLng(this.locations[i].latitud, this.locations[i].longitud),
           map: map,
-          title: 'Uluru'
+          title: this.locations[i].nombre
         });
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
+        marker[i].addListener('click', function() {
+          infowindow.setContent('asdas');
+          infowindow.open(map, marker[i]);
         });
+        marker.push(objMarker);
+      }
+      console.log(marker);
+
     }
-    initMap();
-})();
+  })

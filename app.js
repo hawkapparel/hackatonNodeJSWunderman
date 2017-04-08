6024e32438cef5bb3061777702bc3010cec256da
Christian Tamayo <christianmiguel02@gmail.com>
2017-04-08 00:03:27 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var methodOverride = require("method-override");
+var path = require('path');
+var app = express();
+var fs = require('fs');
+
+// Connection to DB
+mongoose.connect('mongodb://localhost/clients', function(err, res) {
+ if(err) throw err;
+ console.log('Connected to Database');
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
+app.use(methodOverride());
+
+// Import Models and Controllers
+var models     = require('./models/client')(app, mongoose);
+var ClientCtrl = require('./controllers/clients');
+
+var router = express.Router();
+
+var layout = fs.readFileSync('views/index.html', 'utf8')
+
+// Index - Route
+
+app.use(router);
+
+/*
+router.get('/', function(req, res) {  
+   res.render('views/index.html');
+});
+*/
+router.get('/', function(req, res) {
+   res.send(layout);
+});
+
+
+
+
+/*
+router.get('/', function(req, res) {
+    // any logic goes here
+    res.render('views/index')
+});
+*/
+
+// API routes
+var api = express.Router();
+
+api.route('/clients')  
+  .get(ClientCtrl.findAll)
+  .post(ClientCtrl.add);
+
+api.route('/clients/:id')  
+  .get(ClientCtrl.findById)
+  .put(ClientCtrl.update)
+  .delete(ClientCtrl.delete);
+
+app.use('/api', api);  
+
+
+// Start server
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
